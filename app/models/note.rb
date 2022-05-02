c44075b82c2112da26355d38d30d563d468c6c8b
Gala Thevenet <galathevenet@hotmail.com>
2022-05-02 17:00:27 +0200

    model and migration wasn't saved

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,2 +1,4 @@
 class Note < ApplicationRecord
+  validates :name, presence: true
+
 end
--- db/migrate/20220502145441_add_status_to_note.rb
+++ db/migrate/20220502145441_add_status_to_note.rb
@@ -1,5 +1,5 @@
 class AddStatusToNote < ActiveRecord::Migration[6.1]
   def change
-    add_column :notes, :status, :boolean
+    add_column :notes, :status, :boolean, :default => false
   end
 end
